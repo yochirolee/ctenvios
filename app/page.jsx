61da33b67c50304a69343fae4b49879c8d50257d
yochirolee <yleecruz@gmail.com>
2022-11-21 16:16:02 -0500

    card 2

--- app/page.jsx
+++ app/page.jsx
@@ -9,12 +9,12 @@ import { ProductCard } from "../components/ProductCard";
 export default function Page() {
 	return (
 		<>
-			<nav class="flex justify-center" aria-label="Breadcrumb">
+			<nav class="flex ml-4 mb-4 text-xs justify-left" aria-label="Breadcrumb">
 				<ol class="inline-flex justify-center items-center space-x-1 md:space-x-3">
 					<li class="inline-flex items-center">
 						<a
 							href="#"
-							class="inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+							class="inline-flex items-center text-xs font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
 						>
 							<svg
 								class="w-4 h-4 mr-2"
@@ -30,7 +30,7 @@ export default function Page() {
 					<li>
 						<div class="flex items-center">
 							<svg
-								class="w-6 h-6 text-gray-400"
+								class="w-4 h-4 text-gray-400"
 								fill="currentColor"
 								viewBox="0 0 20 20"
 								xmlns="http://www.w3.org/2000/svg"
@@ -43,7 +43,7 @@ export default function Page() {
 							</svg>
 							<a
 								href="#"
-								class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+								class="ml-1 text-xs font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white"
 							>
 								Projects
 							</a>
--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,9 +1,9 @@
 import { FiShoppingCart } from "react-icons/fi";
 export const ProductCard = ({ product }) => {
 	return (
-		<div className="card  shadow-md shrink-0  rounded-lg p-4 w-1/3 lg:w-1/6">
-			<div className=" -mt-8">
-				<img alt="img" className="object-contain h-40 w-40" src="/box.png"></img>
+		<div className="card flex flex-col items-center mt-4 shadow-md shrink-0  rounded-lg p-4 w-1/2 lg:w-1/6">
+			<div className=" -mt-8 ">
+				<img alt="img" className="object-contain h-28 w-28" src="/box.png"></img>
 			</div>
 
 			<div className="flex flex-col  items-center">
@@ -11,16 +11,20 @@ export const ProductCard = ({ product }) => {
 					Caja de Miscelaneas
 				</span>
 				<span className="text-center text-xs mb-3 text-slate-500">Max 6.6 lbs</span>
-				<div className="flex gap-4 items-center">
+				<div className="flex flex-col gap-4 items-center">
 					<span className="text-xs text-center px-2 ring ring-gray-100 bg-gray-500 text-white p-1 rounded-lg ">
-						$ 11
+						Precio: $ 11
 					</span>
-					<span className="relative text-center">
+					<button
+						type="button"
+						class="inline-flex relative items-center p-3 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					>
 						<FiShoppingCart />
-						<span className="-mt-2 text-center align-baseline rounded-full absolute h-4 w-4 bg-red-400 p-1 text-white text-xs">
-							2
-						</span>
-					</span>
+						<span class="sr-only">Cart</span>
+						<div class="inline-flex absolute -top-2 -right-2 justify-center items-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full border-2 border-white dark:border-gray-900">
+							0
+						</div>
+					</button>
 				</div>
 			</div>
 		</div>
