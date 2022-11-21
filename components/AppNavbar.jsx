3a9fea0a82a0a1c081a8e29be72968b497540213
yochirolee <yleecruz@gmail.com>
2022-11-21 12:23:45 -0500

    cards

--- components/AppNavbar.jsx
+++ components/AppNavbar.jsx
@@ -4,15 +4,8 @@ export const AppNavbar = () => {
 	return (
 		<nav x-data="{ isOpen: false }" className="relative bg-white mt-4 dark:bg-gray-800">
 			<div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
-				<div className="flex items-center justify-between">
-					<div>
-						<a
-							className="text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300"
-							href="#"
-						>
-							Brand
-						</a>
-					</div>
+				<div className="flex  justify-between">
+					<img className="h-8 md:h-10 object-scale-down" src="ctelogo.png" alt="logo" />
 
 					<div className="flex lg:hidden">
 						<button
--- components/MainCarousel.jsx
+++ components/MainCarousel.jsx
@@ -3,13 +3,11 @@ import { React } from "react";
 import { Carousel } from "flowbite-react";
 export const MainCarousel = () => {
 	return (
-		<div className="h-56 sm:h-64 xl:h-80 2xl:h-96  ">
+		<div className="px-4 h-56 sm:h-64 xl:h-80 2xl:h-96  ">
 			<Carousel slideInterval={5000}>
-				<img src="Slider1.jpg" alt="..." className="object-scale-down h-screen w-screen" />
-				<img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-				<img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-				<img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-				<img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+				<div className="p-20 lg:p-25 text-white bg-blue-600 h-full w-full">
+					<h1>Envios Maritimos a toda Cuba</h1>
+				</div>
 			</Carousel>
 		</div>
 	);
