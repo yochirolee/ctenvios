5265f58500966484aab233624124b3b9f05fcf20
yochirolee <yleecruz@gmail.com>
2022-11-22 11:14:18 -0500

    banner

--- components/MainCarousel.jsx
+++ components/MainCarousel.jsx
@@ -6,11 +6,16 @@ export const MainCarousel = () => {
 		<div className="px-4 h-56 sm:h-64 xl:h-80 2xl:h-96 ">
 			<Carousel slideInterval={5000}>
 				<div className="bg-[url('/slider4.jpg')] flex   bg-cover bg-center bg-no-repeat p-4 lg:p-25 text-white  h-full w-full">
-					<div className="flex flex-col p-10 m-10 rounded-lg  justify-center lg:pl-20 lg:w-1/3 lg:gap-4 bg-gray-700/40 lg:bg-transparent">
+					<div className="hidden lg:flex lg:flex-col  rounded-lg  justify-center lg:pl-20 lg:w-1/3 lg:gap-4">
 						<h1 className="text-base lg:text-3xl  font-bold">CTEnvios</h1>
 						<h3 className="text-xs lg:text-base">
 							Empresa de Logística con más de 10 años de experiencia en envíos desde Estados Unidos
 							a Latinoamérica y el Caribe
+						</h3>
+					</div>
+					<div className=" lg:hidden text-center flex flex-col mx-10  justify-end mb-6 lg:pl-20 lg:w-1/3 lg:gap-4">
+						<h3 className="text-xs p-1 rounded-lg bg-gray-700/40">
+							Envíos Maritimos a Latinoamérica y el Caribe
 						</h3>
 					</div>
 				</div>
