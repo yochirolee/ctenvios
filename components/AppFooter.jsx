b859a509cdf000024cf854835b6bfb92745ed2f5
yochirolee <yleecruz@gmail.com>
2022-11-22 16:41:32 -0500

    socials

--- components/AppFooter.jsx
+++ components/AppFooter.jsx
@@ -6,44 +6,48 @@ import { React } from "react";
 export const AppFooter = () => {
 	return (
 		<>
-			<Footer bgDark={true}>
-				<div className="w-full">
-					<div className="grid w-full grid-cols-2 gap-8 py-8 px-6 md:grid-cols-4">
-						<div>
-							<Footer.Title title="Company" />
-							<Footer.LinkGroup col={true}>
-								<Footer.Link href="#">About</Footer.Link>
-								<Footer.Link href="#">Careers</Footer.Link>
-								<Footer.Link href="#">Brand Center</Footer.Link>
-								<Footer.Link href="#">Blog</Footer.Link>
-							</Footer.LinkGroup>
-						</div>
-						<div>
-							<Footer.Title title="help center" />
-							<Footer.LinkGroup col={true}>
-								<Footer.Link href="#">Instagram</Footer.Link>
-								<Footer.Link href="#">Facebook</Footer.Link>
-								<Footer.Link href="#">Contact Us</Footer.Link>
-							</Footer.LinkGroup>
-						</div>
-						<div>
-							<Footer.Title title="legal" />
-							<Footer.LinkGroup col={true}>
-								<Footer.Link href="#">Privacy Policy</Footer.Link>
-								<Footer.Link href="#">Licensing</Footer.Link>
-								<Footer.Link href="#">Terms & Conditions</Footer.Link>
-							</Footer.LinkGroup>
-						</div>
-					</div>
-					<div className="w-full bg-gray-700 py-6 px-4 sm:flex sm:items-center sm:justify-between">
-						<Footer.Copyright href="#" by="CTEnvios" year={2022} />
-						<div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-							<Footer.Icon href="#" icon={BsFacebook} />
-							<Footer.Icon href="#" icon={BsInstagram} />
-						</div>
-					</div>
+			<footer class="p-4 bg-white rounded-lg  md:px-6 md:py-8 dark:bg-gray-900">
+				<div class="sm:flex sm:items-center sm:justify-between">
+					<a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+						<img
+							src="ctelogo.png"
+							class="mr-3 h-8"
+							alt="Logo"
+						/>
+						
+					</a>
+					<ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+						<li>
+							<a href="#" class="mr-4 hover:underline md:mr-6 ">
+								About
+							</a>
+						</li>
+						<li>
+							<a href="#" class="mr-4 hover:underline md:mr-6">
+								Privacy Policy
+							</a>
+						</li>
+						<li>
+							<a href="#" class="mr-4 hover:underline md:mr-6 ">
+								Licensing
+							</a>
+						</li>
+						<li>
+							<a href="#" class="hover:underline">
+								Contact
+							</a>
+						</li>
+					</ul>
 				</div>
-			</Footer>
+				<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+				<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+					© 2022
+					<a href="https://ctenvios.vercel.app" class="hover:underline">
+						CTEnvios™
+					</a>
+					. All Rights Reserved.
+				</span>
+			</footer>
 		</>
 	);
 };
