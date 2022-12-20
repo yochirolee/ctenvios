17417a0e345739c531b4a13d99f6608c9aa78e4b
yochirolee <yleecruz@gmail.com>
2022-12-20 17:03:38 -0500

    faq 23

--- Helpers/dbViews/fethInvoicesFromViewByInvoiceId.js
+++ Helpers/dbViews/fethInvoicesFromViewByInvoiceId.js
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const fetchInvoicesFromViewsByInvoiceId = async (InvoiceId) => {
 	const { data } = await axios.get("https://caribe-cargo-api.vercel.app/api/items/" + InvoiceId);
-    console.log(data.data,"FETCHING DATA")
 	return data.data;
 };
