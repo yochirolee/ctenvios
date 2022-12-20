13548425f9a2fc6265c662bc6cbb86a18ebd83ad
yochirolee <yleecruz@gmail.com>
2022-12-20 14:52:44 -0500

    faq 22

--- app/page.jsx
+++ app/page.jsx
@@ -8,49 +8,6 @@ import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
 export default function Page() {
 	return (
 		<>
-			<nav className=" flex ml-4 mb-4 text-xs justify-left" aria-label="Breadcrumb">
-				<ol className="inline-flex justify-center items-center space-x-1 md:space-x-3">
-					<li className="inline-flex items-center">
-						<a
-							href="#"
-							className="inline-flex items-center text-xs font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
-						>
-							<svg
-								className="w-4 h-4 mr-2"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-							</svg>
-							Home
-						</a>
-					</li>
-					<li>
-						<div className="flex items-center">
-							<svg
-								className="w-4 h-4 text-gray-400"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-									clip-rule="evenodd"
-								></path>
-							</svg>
-							<a
-								href="#"
-								className="ml-1 text-xs font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white"
-							>
-								Projects
-							</a>
-						</div>
-					</li>
-				</ol>
-			</nav>
-
 			<MainCarousel />
 
 			<Products />
