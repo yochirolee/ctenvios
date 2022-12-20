6808117d5dbb5a7dbd03f6494b71a24f4b6aad94
yochirolee <yleecruz@gmail.com>
2022-12-20 14:51:56 -0500

    faq

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -28,47 +28,6 @@ export const Faq = () => {
 									puntos en total, los cuales incluye tanto el valor de lo que se envia, como el
 									equipaje del pasajero que viajara por el aeropuerto.{" "}
 								</p>
-								
-							</Accordion.Content>
-						</Accordion.Panel>
-						<Accordion.Panel>
-							<Accordion.Title>
-								What are the differences between Flowbite and Tailwind UI?
-							</Accordion.Title>
-							<Accordion.Content>
-								<p className="mb-2 text-gray-500 dark:text-gray-400">
-									The main difference is that the core components from Flowbite are open source
-									under the MIT license, whereas Tailwind UI is a paid product. Another difference
-									is that Flowbite relies on smaller and standalone components, whereas Tailwind UI
-									offers sections of pages.
-								</p>
-								<p className="mb-2 text-gray-500 dark:text-gray-400">
-									However, we actually recommend using both Flowbite, Flowbite Pro, and even
-									Tailwind UI as there is no technical reason stopping you from using the best of
-									two worlds.
-								</p>
-								<p className="mb-2 text-gray-500 dark:text-gray-400">
-									Learn more about these technologies:
-								</p>
-								<ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-									<li>
-										<a
-											href="https://flowbite.com/pro/"
-											className="text-blue-600 hover:underline dark:text-blue-500"
-										>
-											Flowbite Pro
-										</a>
-									</li>
-									<li>
-										<a
-											href="https://tailwindui.com/"
-											rel="nofollow"
-											className="text-blue-600 hover:underline dark:text-blue-500"
-										>
-											Tailwind UI
-										</a>
-									</li>
-								</ul>
 							</Accordion.Content>
 						</Accordion.Panel>
 					</Accordion>
